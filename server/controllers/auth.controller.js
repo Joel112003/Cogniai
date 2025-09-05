e4dbd4ed97da8b7ef1b8f3469905fdc589ef3fd4
joel <155870458+Joel112003@users.noreply.github.com>
2025-09-06 00:25:33 +0530

    updated folders

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,6 @@
+import bycrpt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+// import User from '../models/User.js';
+// import Role from '../models/Role.js';
+// import { validationResult } from 'express-validator';
+// import { secret } from '../config.js';
